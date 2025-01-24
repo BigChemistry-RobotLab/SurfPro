6830fdedd4ce2b1fd15100b89943a6cb353541cc
Stefan Hödl <stefan.hoedl@ru.nl>
2025-01-24 10:48:54 +0100

    finishes plotting script for distribution by type for each prop

--- scripts/plot_distributions.py
+++ scripts/plot_distributions.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# from src.converter import map_smiles_to_class
+from matplotlib.patches import Patch
 from omegaconf import OmegaConf
-import dvc
+
 
 def map_surfactant_type(stype):
     type_map = {
@@ -18,110 +18,73 @@ def map_surfactant_type(stype):
     }
     return type_map.get(stype)
 
+
 # @hydra.main(version_base="1.3", config_path="../conf", config_name="config")
 def plot_distributions():  #cfg: DictConfig) -> None:
     print("PLOT CONFIG from params.yaml")
     cfg = OmegaConf.load("./params.yaml")
     print(OmegaConf.to_yaml(cfg))
 
-
     df = pd.read_csv(f'{cfg.host.workdir}/data/surfpro_literature.csv')
     df['type'] = df['Surfactant_Type'].apply(map_surfactant_type)
 
-    types, types_count = np.unique(df.type, return_counts=True)
-    print(df.type)
-    print('\noverall counts:')
-    [print(k,v) for k,v in list(zip(types, types_count))]
+    # types, types_count = np.unique(df.type, return_counts=True)
+    # print('\noverall counts:')
+    # [print(k,v) for k,v in list(zip(types, types_count))]
+    types = ['gemini cationic', 'cationic', 'non-ionic', 'anionic']
 
     properties = ['pCMC', 'AW_ST_CMC', 'Gamma_max', 'pC20']
     colormap = {
+        "full dataset": 'grey',
         "gemini cationic": 'tab:red',
         "cationic": 'tab:orange',
         "anionic": 'tab:green',
-        "non-ionic": 'tab:blue', 
+        "non-ionic": 'tab:blue',
         # "zwitterionic": 'tab:purple',
     }
 
-    for property in properties: 
+    for property in properties:
 
-        fig, axes = plt.subplots(3, 2)
-        assert len(types) == 5
-        all_types = df.loc[:, property]
+        fig, axes = plt.subplots(2, 2, figsize=(18, 10), sharex=False, sharey=True)  
+        axes = axes.flatten()
         for i, stype in enumerate(types):
-            if stype != 'zwitterionic':
-                ax = axes.flatten[i]
-                print('\n', property, stype)
-                all_df = df.loc[:, ['SMILES', 'type', property]]
-                all_df = all_df.dropna(axis=0).reset_index(drop=True)
-                # sub_df = all_df.loc[all_df.where(all_df.loc[:, 'type'] == stype), :]
-                sub_df = all_df.iloc[np.where(all_df['type'] == stype)]
-                print(all_df.shape)
-                print(sub_df.shape)
-                print(sub_df)
+            # if stype != 'zwitterionic':
+            ax = axes[i]
+            all_df = df.loc[:, ['SMILES', 'type', property]]
+            all_df = all_df.dropna(axis=0).reset_index(drop=True)
+            sub_df = all_df.iloc[np.where(all_df['type'] == stype)]
 
-                ax.hist(all_df.loc[:, 'property'], bins=20, label=stype, 
-                        alpha = 0.3, color = cmap[stype], edgecolor='black')
-                ax.hist(all_df.loc[:, 'property'], bins=20, label=stype, 
-                        alpha = 0.7, color = cmap[stype], edgecolor='black')
+            print('\n', property, stype)
+            print(all_df.shape)
+            print(sub_df.shape)
 
-        # ax.set_title(f'{property}: {stype} ', fontsize=15)
-        ax.set_xlabel(prop, fontsize=14)
-        ax.set_ylabel('Frequency', fontsize=14)
-        ax.grid(axis='y', alpha=0.75)
+            ax.hist(all_df.loc[:, property], bins=30,
+                    alpha = 0.3, color = 'grey', edgecolor='black')
+            ax.hist(sub_df.loc[:, property], bins=30,
+                    alpha = 0.7, color = colormap[stype], edgecolor='black')
 
-    plt.tight_layout()
-    plt.legend()
-    plt.show()
+            if i % 2 == 0:
+                ax.set_ylabel('Frequency', fontsize=18)
+            ax.set_xlabel(stype, fontsize=18)
+            ax.grid(axis='y', alpha=0.75)
+            ax.set_xlim(0, 7)
+            ax.set_ylim(0, 140)
+            ax.tick_params(axis="x", labelsize=13)
+            ax.tick_params(axis="y", labelsize=13)
+
+        legend_patches = [
+            Patch(facecolor=color, label=label, alpha=0.7)
+            for label, color in colormap.items()
+        ]
+        axes[1].legend(handles=legend_patches, fontsize=18, loc="upper right")
+        plt.suptitle(f'Comparison of {property} counts by surfactant type', fontsize=20)
+        plt.tight_layout()
+        plt.savefig(
+            f"{cfg.host.workdir}/results/plots/data_distrib_hist_{property}.png",
+            dpi=300,
+            bbox_inches="tight",
+        )
 
 
 if __name__ == "__main__":
     plot_distributions()
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#
-#
-#
-#
-# properties = ['pCMC', 'T']  # Assuming 'T' is one of the properties (temperature)
-# categories = data['category'].unique()
-#
-# # Create subplots, one for each category
-# fig, axes = plt.subplots(len(categories), len(properties), figsize=(15, 5 * len(categories)))
-#
-# # Ensure axes is iterable in case of a single subplot
-# if len(categories) == 1:
-#     axes = [axes]
-#
-# for i, category in enumerate(categories):
-#     subset = data[data['category'] == category]
-#
-#     for j, prop in enumerate(properties):
-#         ax = axes[i][j] if len(properties) > 1 else axes[i]
-#         # Plot the entire dataset in semi-transparent
-#         ax.hist(data[prop], bins=20, alpha=0.3, color='gray', label='All Data', edgecolor='black')
-#
-#         # Overlay the subset in full color
-#         ax.hist(subset[prop], bins=20, alpha=0.7, label=f'{category}', edgecolor='black')
-#
-#         # Add titles and labels
-#         ax.set_title(f'{category} - {prop}', fontsize=14)
-#         ax.set_xlabel(prop, fontsize=12)
-#         ax.set_ylabel('Frequency', fontsize=12)
-#         ax.grid(axis='y', alpha=0.75)
-#
-# # Adjust layout and show legend
-# plt.tight_layout()
-# plt.legend()
-# plt.show()
-#
